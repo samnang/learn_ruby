b64296baa15df2f5cde9c53570962e52dbf0ef88
Samnang Chhun <samnang.chhun@gmail.com>
2012-08-08 22:51:08 +0700

    Finish 02_calculator with bonus factorial

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,25 @@
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(numbers)
+  numbers.inject(&:+) || 0
+end
+
+def multiply(numbers)
+  numbers.inject(&:*)
+end
+
+def power(number)
+  number ** 2
+end
+
+def factorial(n)
+  return 1 if [0, 1].include?(n)
+
+  n * factorial(n - 1)
+end
