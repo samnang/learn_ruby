34a5f7d469e049ed817ae2f2115c0dfc4d163932
Samnang Chhun <samnang.chhun@gmail.com>
2012-08-13 22:55:47 +0700

    Finish 11_dictionary

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,31 @@
+class Dictionary
+  attr_reader :entries
+
+  def initialize
+    @entries = {}
+  end
+
+  def add(args)
+    if args.is_a? Hash 
+      entries.merge!(args)
+    else
+      entries[args] = nil
+    end
+  end
+
+  def keywords
+    entries.keys.sort
+  end
+
+  def include?(key)
+    entries.key?(key)
+  end
+
+  def find(key)
+    entries.select { |k, _| k.start_with?(key) }
+  end
+
+  def printable
+    keywords.map { |k| "[%s] %s" % [k, entries[k].inspect] }.join("\n")
+  end
+end
